d43afdc9cd8a1c94c9da92769e93e03d97bb438e
adinaamzarescu <73987572+adinaamzarescu@users.noreply.github.com>
2022-01-17 12:54:44 +0200

    Constants class

---
+++ common/Constants.java
@@ -0,0 +1,30 @@
+package common;
+
+public final class Constants {
+    public static final String OUTPUT_PATH = "output/out_";
+    public static final String REF_PATH = "ref/ref_test";
+    public static final String FILE_EXTENSION = ".json";
+    public static final Integer SMALL_TEST_POINTS = 2;
+    public static final Integer BIG_TEST_POINTS = 3;
+    public static final Integer TESTS_NUMBER = 25;
+    public static final Integer MAXIMUM_ERROR_CHECKSTYLE = 30;
+    public static final Integer CHECKSTYLE_POINTS = 10;
+    public static final Integer TESTS_NUMBER_SMALL = 15;
+    public static final Integer FIVE = 5;
+    public static final Integer TWELVE = 12;
+    public static final Integer EIGHTEEN = 18;
+    public static final Double BABY_GRADE = 10.0;
+    public static final String TESTS_PATH = "tests/";
+    public static final String RESULT_PATH = "output/";
+    public static final String OUT_FILE = "out.txt";
+    public static final int FOUR = 4;
+    public static final int HUNDRED = 100;
+    public static final int THIRTY = 30;
+    public static final int TEN = 10;
+
+
+    private Constants() {
+        //constructor for Constants
+    }
+
+}
