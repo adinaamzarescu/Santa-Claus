43ab06b7b04edb2c0283654afbf7b8d58bbf8900
adinaamzarescu <73987572+adinaamzarescu@users.noreply.github.com>
2022-01-22 18:22:48 +0200

    Fixed coding style errors

--- changes/AnnualChanges.java
+++ changes/AnnualChanges.java
@@ -1,15 +1,26 @@
 package changes;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.ArrayList;
 
-@Getter
-@Setter
-public class AnnualChanges {
+public final class AnnualChanges {
     private Integer numberOfYears;
     private ArrayList<Class<ChangesList>> annualChanges;
+
+    public ArrayList<Class<ChangesList>> getAnnualChanges() {
+        return annualChanges;
+    }
+
+    public Integer getNumberOfYears() {
+        return numberOfYears;
+    }
+
+    public void setAnnualChanges(final ArrayList<Class<ChangesList>> annualChanges) {
+        this.annualChanges = annualChanges;
+    }
+
+    public void setNumberOfYears(final Integer numberOfYears) {
+        this.numberOfYears = numberOfYears;
+    }
 
     /**
      * Constructor
--- changes/ChangesList.java
+++ changes/ChangesList.java
@@ -1,15 +1,44 @@
 package changes;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.ArrayList;
 
-@Getter
-@Setter
-public class ChangesList {
+
+public final class ChangesList {
     private Double newSantaBudget;
+
     private ArrayList<String> newGifts;
     private ArrayList<String> newChildren;
     private ArrayList<String> childrenUpdates;
+
+    public void setNewGifts(final ArrayList<String> newGifts) {
+        this.newGifts = newGifts;
+    }
+
+    public void setNewChildren(final ArrayList<String> newChildren) {
+        this.newChildren = newChildren;
+    }
+
+    public void setChildrenUpdates(final ArrayList<String> childrenUpdates) {
+        this.childrenUpdates = childrenUpdates;
+    }
+
+    public void setNewSantaBudget(final Double newSantaBudget) {
+        this.newSantaBudget = newSantaBudget;
+    }
+
+    public Double getNewSantaBudget() {
+        return newSantaBudget;
+    }
+
+    public ArrayList<String> getNewGifts() {
+        return newGifts;
+    }
+
+    public ArrayList<String> getNewChildren() {
+        return newChildren;
+    }
+
+    public ArrayList<String> getChildrenUpdates() {
+        return childrenUpdates;
+    }
 }
